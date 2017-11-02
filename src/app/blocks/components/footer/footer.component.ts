da86484fe93bab962a89eaeacc2692a593b9c5e4
blaze1591 <alex.kozhuchar@gmail.com>
2017-11-02 22:18:46 +0200

    Some changes

--- src/app/blocks/components/footer/footer.component.ts
+++ src/app/blocks/components/footer/footer.component.ts
@@ -4,9 +4,11 @@ import {Component} from '@angular/core';
   selector: 's2m-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
-    <div class="created-by">Розробили: <b>Кожухар Олександр</b> та <b>Сальский Дмитро</b> 2017</div>
-    <div class="socials">
-      <a href="https://github.com/blaze1591/s2m-fe" target="_blank" class="ion ion-social-github"></a>
+    <div>
+      <div class="created-by">Розробили: <b>Кожухар Олександр</b> та <b>Сальский Дмитро</b> 2017</div>
+      <div class="socials">
+        <a href="https://github.com/blaze1591/s2m-fe" target="_blank" class="ion ion-social-github"></a>
+      </div>
     </div>
   `,
 })
