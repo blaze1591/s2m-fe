4c1c985f5c779609a402d0ed30e7a891a329a602
Alexander <alex.kozhuchar@gmail.com>
2017-12-09 21:11:07 +0200

    Markups for employees and publications

--- src/app/pages/employees/employees.component.ts
+++ src/app/pages/employees/employees.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {LocalDataSource} from 'ng2-smart-table';
 
 @Component({
   selector: 's2m-employees',
@@ -7,10 +8,81 @@ import {Component, OnInit} from '@angular/core';
 })
 export class EmployeesComponent implements OnInit {
 
+  settings = {
+    actions: {
+      columnTitle: 'Дії',
+    },
+    add: {
+      addButtonContent: '<i class="nb-plus"></i>',
+      createButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    edit: {
+      editButtonContent: '<i class="nb-edit"></i>',
+      saveButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    delete: {
+      deleteButtonContent: '<i class="nb-trash"></i>',
+      confirmDelete: true,
+    },
+    noDataMessage: 'Нема даних',
+    columns: {
+      fioUkr: {
+        title: 'ФИО(укр)',
+        type: 'string',
+      },
+      email: {
+        title: 'Пошта',
+        type: 'string',
+      },
+      degree: {
+        title: 'Наукова ступінь',
+        type: 'string',
+      },
+      academicTitle: {
+        title: 'Вчене звання',
+        type: 'string',
+      },
+      cathedras: {
+        title: 'Кафедры',
+        type: 'string',
+      },
+    },
+  };
+
+  source: LocalDataSource;
+
   constructor() {
+    this.source = new LocalDataSource([]);
   }
 
   ngOnInit() {
+  }
+
+  onSearch(query: string = '') {
+    this.source.setFilter([
+      {
+        field: 'fioUkr',
+        search: query,
+      },
+      {
+        field: 'email',
+        search: query,
+      },
+      {
+        field: 'degree',
+        search: query,
+      },
+      {
+        field: 'academicTitle',
+        search: query,
+      },
+      {
+        field: 'cathedras',
+        search: query,
+      },
+    ]);
   }
 
 }
--- src/app/pages/publications/publications.component.ts
+++ src/app/pages/publications/publications.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {LocalDataSource} from 'ng2-smart-table';
 
 @Component({
   selector: 's2m-publications',
@@ -7,10 +8,72 @@ import {Component, OnInit} from '@angular/core';
 })
 export class PublicationsComponent implements OnInit {
 
+  settings = {
+    actions: {
+      columnTitle: 'Дії',
+    },
+    add: {
+      addButtonContent: '<i class="nb-plus"></i>',
+      createButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    edit: {
+      editButtonContent: '<i class="nb-edit"></i>',
+      saveButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    delete: {
+      deleteButtonContent: '<i class="nb-trash"></i>',
+      confirmDelete: true,
+    },
+    noDataMessage: 'Нема даних',
+    columns: {
+      title: {
+        title: 'Назва',
+        type: 'string',
+      },
+      type: {
+        title: 'Тип',
+        type: 'string',
+      },
+      author: {
+        title: 'Автор',
+        type: 'string',
+      },
+      date: {
+        title: 'Дата',
+        type: 'string',
+      },
+    },
+  };
+
+  source: LocalDataSource;
   constructor() {
+    this.source = new LocalDataSource([]);
   }
 
   ngOnInit() {
+  }
+
+  onSearch(query: string = '') {
+    this.source.setFilter([
+      {
+        field: 'title',
+        search: query,
+      },
+      {
+        field: 'type',
+        search: query,
+      },
+      {
+        field: 'author',
+        search: query,
+      },
+      {
+        field: 'date',
+        search: query,
+      },
+    ]);
   }
 
 }
