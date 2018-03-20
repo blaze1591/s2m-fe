9e8d6b5f2dad3099a429bc6178db175183ac0f79
blaze1591 <alex.kozhuchar@gmail.com>
2018-03-20 17:08:58 +0200

    creative commons image and date update

--- src/app/blocks/components/footer/footer.component.ts
+++ src/app/blocks/components/footer/footer.component.ts
@@ -6,10 +6,10 @@ import {Component} from '@angular/core';
   template: `
     <div>
       <div class="created-by">
-        Розробили: <b>Кожухар Олександр</b> та <b>Сальский Дмитро</b> 2017
+        Розробили: <b>Кожухар Олександр</b> та <b>Сальский Дмитро</b> 2015
         <span>
           <img alt="Creative Commons License" style="border-width:0"
-               src="https://i.creativecommons.org/l/by/4.0/88x31.png"/>
+               src="https://licensebuttons.net/l/by-nc/4.0/88x31.png"/>
         </span>
       </div>
     </div>
